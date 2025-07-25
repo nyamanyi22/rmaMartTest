44891bb0c64e11e0daecadd251361ed3cb1dbd69
Elizabeth Nyamanyi <elizabethomari03@gmail.com>
2025-07-25 07:27:39 +0300

    friday

--- src/Admin/pages/customers/EditCustomer.jsx
+++ src/Admin/pages/customers/EditCustomer.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import AdminCustomerForm from './AdminCustomerForm';
-i
+
 import MessageModal from '../../Components/MessageModal';
 
 const EditCustomer = () => {
